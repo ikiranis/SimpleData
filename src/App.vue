0d3d33b25f034f63141470b4be1aae15d4ba814e
rocean <rocean74@gmail.com>
2020-09-17 02:09:58 +0300

    Display data

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,7 @@
 
     <save-file :data="data"/>
 
-    <div v-for="item in data" :key="item.id">
-        <div>{{ item.id }}</div>
-        <div>{{ item.name }}</div>
-        <div>{{ item.city }}</div>
-    </div>
+    <display-data :data="data"/>
 
 
 </template>
@@ -15,18 +11,20 @@
 <script>
 
 	import {ref} from 'vue'
-	import OpenFile from './components/OpenFile.vue'
+	import OpenFile from './components/OpenFile'
 	import SaveFile from "./components/SaveFile";
+	import DisplayData from "./components/DisplayData";
 
 	export default {
 		name: 'App',
 		components: {
 			SaveFile,
-			OpenFile
+			OpenFile,
+			DisplayData
 		},
 
 		setup() {
-			const data = ref( null)
+			const data = ref(null)
 
 			return {
 				data
---
+++ src/components/DisplayData.vue
@@ -0,0 +1,30 @@
+<template>
+    <table class="table mt-5">
+        <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">City</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in data" :key="item.id">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.city }}</td>
+            </tr>
+        </tbody>
+    </table>
+
+</template>
+
+<script>
+	export default {
+		props: {
+			data: {
+				required: true,
+				type: Array
+			}
+		}
+	}
+</script>
