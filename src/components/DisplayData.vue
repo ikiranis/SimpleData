d5647c9d161bea1dc8e1ec6697b63809b08a45e2
rocean <rocean74@gmail.com>
2020-09-19 03:38:11 +0300

    Dynamic display fields

--- src/components/DisplayData.vue
+++ src/components/DisplayData.vue
@@ -2,16 +2,15 @@
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">id</th>
-                <th scope="col">Name</th>
-                <th scope="col">City</th>
+                <th v-for="field in fields"
+                    :key="field.fieldId"
+                    scope="col">{{ field.fieldId }}</th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="item in data" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.city }}</td>
+                <td v-for="field in fields"
+                    :key="field.fieldId">{{ item[field.fieldId] }}</td>
             </tr>
         </tbody>
     </table>
@@ -19,12 +18,22 @@
 </template>
 
 <script>
+	import { useFields } from '@/functions/fields'
+
 	export default {
 		props: {
 			data: {
 				required: true,
 				type: Array
 			}
-		}
+		},
+
+        setup() {
+			const { fields, emptyRecord } = useFields()
+
+            return {
+				fields, emptyRecord
+            }
+        }
 	}
 </script>
