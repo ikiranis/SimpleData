93a6870170fb4fbe80417a9fb905a48d540d54f4
rocean <rocean74@gmail.com>
2020-09-19 03:00:08 +0300

    Dynamic input fields from json

--- src/components/InputData.vue
+++ src/components/InputData.vue
@@ -1,15 +1,11 @@
 <template>
     <div class="mx-auto col-lg-6 col-12">
-        <div class="mb-3">
-            <text-input fieldId="name" labelText="Name" inputLength=20
-                        :modelValue="record.name"
-                        @update:modelValue="record.name = $event" />
-        </div>
 
-        <div class="mb-3">
-            <text-input fieldId="city" labelText="City" inputLength=20
-                        :modelValue="record.city"
-                        @update:modelValue="record.city = $event" />
+
+        <div class="mb-3" v-for="field in fields" :key="field.fieldId">
+            <text-input :fieldId="field.fieldId" :labelText="field.labelText" :inputLength="field.inputLength"
+                        :modelValue="record[field.fieldId]"
+                        @update:modelValue="record[field.fieldId] = $event" />
         </div>
 
         <button @click="addRecord" class="btn btn-warning mt-3 col-lg-6 col-12">Προσθήκη</button>
@@ -28,11 +24,25 @@
 		},
 
 		setup(props, {emit}) {
-			const emptyRecord = {
-				id: null,
-				name: '',
-				city: ''
-			}
+			// TODO read this from file
+			const fields = [
+				{
+					fieldId: 'name',
+					labelText: 'Name',
+					inputLength: 20
+				},
+				{
+					fieldId: 'city',
+					labelText: 'City',
+					inputLength: 20
+				},
+			]
+
+			let emptyRecord = {}
+
+			fields.forEach(item => {
+				emptyRecord[item.fieldId] = null
+			})
 
 			let record = ref({emptyRecord})
 
@@ -44,6 +54,7 @@
 			return {
 				record,
 				addRecord,
+                fields
 			}
 		}
 	}
