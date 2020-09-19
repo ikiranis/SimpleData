d7afac822db18289a9d3b054fb46daebc74ead8c
rocean <rocean74@gmail.com>
2020-09-20 02:06:38 +0300

    Read json from file

--- src/functions/fields.js
+++ src/functions/fields.js
@@ -2,21 +2,11 @@ import { ref } from 'vue'
 
 export const useFields = () => {
 
-	// TODO read this from file
-	const fields = ref([
-		{
-			id: 'name',
-			labelText: 'Name',
-			inputLength: 20
-		},
-		{
-			id: 'city',
-			labelText: 'City',
-			inputLength: 20
-		},
-	])
+	let json = require('@/fields.json')
 
-	let emptyRecord = ref({})
+	const fields = ref(json)
+
+	let emptyRecord = ref({id: null})
 
 	fields.value.forEach(item => {
 		emptyRecord.value[item.id] = null
